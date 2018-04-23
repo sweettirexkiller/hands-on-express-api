a1ee73ff0da38a50dd512c04d438e15bb30fe251
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-23 15:18:09 +0200

    header and CORS, access-allow method,headers,origin

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use((req,res,next)=>{
+   res.header("Access-Control-Allow-Origin", '*');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-type, Accept, Authorization");
+   if(req.method === "OPTIONS"){
+       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATH, DELETE, GET');
+       return res.status(200).json({});
+   }
+   next()
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
