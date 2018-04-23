5db4efaa58f1ef9797bd94c569db8ab4e249ab1e
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-23 14:28:30 +0200

    bodyParser is included in express by deafault

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
