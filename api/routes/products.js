c18dedc72479213bc9ada4dc4f4b6c8a4bbc4e22
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-23 16:37:36 +0200

    mongoose and all routes for crud

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,53 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "Handling GET request to /products"
-    });
+    Product.find().exec()
+        .then(docs => {
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            res.status(500).json({err});
+        });
+
 });
 
 
 router.post("/", (req, res, next) => {
-    const product = {
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(201).json({
-        message: "Handling POST request to /products",
-        product: product
     });
+    product.save()
+        .then(result => {
+            res.status(201).json({
+                product: result
+            });
+        })
+        .catch(error => {
+            res.status(500).json({error});
+        });
+
 });
 
 router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: "You discovered a special ID",
-            id: id
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({message: 'No valid entry found for provided ID'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: err});
         });
-    } else {
-        res.status(200).json({
-            message: "You passed an ID"
-        });
-    }
-
 });
 
 router.patch("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: "Updated product",
-        id: id
-    });
+    const updateOps = {};
+    for (const key of Object.keys(req.body)) {
+        updateOps[key] = req.body[key]
+    }
+    Product.update({_id: id}, {$set: updateOps}).exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(error => {
+            res.status(500).json({error})
+        });
+
 });
 
 router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: "Deleted product",
-        id: id
-    });
+    Product.remove({_id: id}).exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(error => {
+            res.status(500).json({error})
+        });
 });
 
 
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
@@ -8,14 +9,16 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use((req,res,next)=>{
-   res.header("Access-Control-Allow-Origin", '*');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-type, Accept, Authorization");
-   if(req.method === "OPTIONS"){
-       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATH, DELETE, GET');
-       return res.status(200).json({});
-   }
-   next()
+mongoose.connect(`mongodb://node-shop:${process.env.MONGO_ATLAS_PW}@node-shop-shard-00-00-gpxsx.mongodb.net:27017,node-shop-shard-00-01-gpxsx.mongodb.net:27017,node-shop-shard-00-02-gpxsx.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-shard-0&authSource=admin`, {});
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next()
 });
 
 app.use('/products', productRoutes);
