349433e781d181b4e516fa7c2fcf2dee1b18abc0
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-24 17:40:10 +0200

    work on tokens

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', (req, res, next) => {
     User.find({email: req.body.email})
@@ -45,17 +46,55 @@ router.post('/signup', (req, res, next) => {
         });
 });
 
+router.post('/login', (req, res, next) => {
+    User.find({email: req.body.email}).exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                        );
+                    res.status(200).json({
+                        message: "Auth successful",
+                        token: token
+                    })
+                } else {
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                }
+            })
+        })
+        .catch(err => {
+            return res.status(500).json({
+                error: err
+            });
+        });
+});
+
 router.delete('/:userId', (req, res, next) => {
     User.remove({_id: req.params.userId})
         .exec()
-        .then(result =>{
+        .then(result => {
             res.status(200).json({
                 message: "User deleted"
             })
         })
         .catch(err => {
             res.status(500).json({
-                error:err
+                error: err
             });
         });
 });
